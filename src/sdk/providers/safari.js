d1c464f80b74763ab450c2e439705c884eba602f
Maxim Lanin <max@lanin.me>
2016-07-18 12:52:43 +0300

    Add unsupported check

--- src/sdk/providers/safari.js
+++ src/sdk/providers/safari.js
@@ -19,6 +19,12 @@ const provider = class Safari extends AbstractProvider {
    * @return {Boolean}
    */
   pushSupported () {
+    if (typeof this.options.safariWebId === 'undefined') {
+      Notimatica.emit('warning', 'Safari is unsupported due to lack of safariWebId in Notimatica options.')
+
+      return false
+    }
+
     return 'safari' in window && 'pushNotification' in window.safari
   }
 
