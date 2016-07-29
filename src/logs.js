fff6f61f66b146cbf994e76e02a070059cc786fd
Maxim Lanin <max@lanin.me>
2016-07-29 18:57:36 +0300

    Add style to logging

--- src/logs.js
+++ src/logs.js
@@ -3,11 +3,21 @@ import { isString } from './utils'
 export default function Logs (target, prefix, style) {
   target = target || this
 
-  target.log = function (type, context) {
+  target.log = function (type, args) {
     if (!target._debug) return
 
-    if (isString(context[0])) context[0] = prefix + context[0]
-    console[type].apply(console, context)
+    args = Array.prototype.slice.call(args)
+    if (isString(args[0]) && prefix) {
+      let message = args.shift()
+      message = [
+        `%c${prefix}%c` + message,
+        'color:#fdbd2c;font-weight:bold;',
+        'color:black;font-weight:normal;'
+      ]
+      args = message.concat(args)
+    }
+
+    console[type].apply(console, args)
   }
 
   target.debug = function () {
